112a6d985654c92a0cc2c5fb305ee9925d709f25
Guilherme Menoci <guimenoci@gmail.com>
2021-11-27 17:38:53 -0300

    Implementação de logs do Evento

--- src/main/java/br/com/magna/api/ApiApplication.java
+++ src/main/java/br/com/magna/api/ApiApplication.java
@@ -1,14 +1,20 @@
 package br.com.magna.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@EnableSpringDataWebSupport
 public class ApiApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(ApiApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Iniciando API");
 		SpringApplication.run(ApiApplication.class, args);
+		logger.info("API iniciada");
 	}
 
 }
--- src/main/java/br/com/magna/api/controller/EventoController.java
+++ src/main/java/br/com/magna/api/controller/EventoController.java
@@ -3,6 +3,8 @@ package br.com.magna.api.controller;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Page;
@@ -19,54 +21,68 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.magna.api.ApiApplication;
 import br.com.magna.api.dto.EventoDto;
 import br.com.magna.api.service.EventoService;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/eventos")
 public class EventoController {
+	
+	private static Logger logger = LoggerFactory.getLogger(ApiApplication.class);
 
 	@Autowired
 	private EventoService eventoService;
-
-	// Listando usuario por CODIGO
+	
+	@ApiOperation("Listando eventos com codigo")
+	// Listando eventos por CODIGO
 	@GetMapping("/{codigo}")
 	public ResponseEntity<EventoDto> listCodigo(@PathVariable Long codigo) throws NotFoundException {
 		try {
+			logger.info("Evento com codigo: " + codigo + " encontrado");
 			return ResponseEntity.ok(eventoService.getCodigo(codigo));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.out.println("Usuario não encontrado");
+			logger.info("Evento com codigo: " + codigo + " não encontrado");
 			return ResponseEntity.notFound().build();
 		}
 	}
 	
+	@ApiOperation("Listando eventos com Page")
 	// Listando evento com Page e ordem crescente
 	@GetMapping
 	public Page<EventoDto> listCodigo(@RequestParam(required = false) Long codigo, Pageable pagina)
 			throws NotFoundException {
 		try {
+			logger.info("Eventos: " + pagina);
 			return eventoService.listPage(codigo, pagina);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.out.println("Usuario não encontrado");
+			logger.info("Eventos não encontrados");
 			return null;
 		}
 	}
 
+	@ApiOperation("Adicionando eventos")
 	// Adicionando eventos
 	@PostMapping
 	public ResponseEntity<EventoDto> post(@RequestBody @Valid EventoDto eventoDto) throws NotFoundException {
 		try {
 			EventoDto eventoDtoCreate = eventoService.createEventoDto(eventoDto);
+			logger.info("Evento cadastrado");
 			return ResponseEntity.status(HttpStatus.CREATED).body(eventoDtoCreate);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.out.println("Evento não cadastrado");
+			logger.info("Evento não cadastrado");
 			return ResponseEntity.noContent().build();
 		}
 	}
 
+	@ApiOperation("Atualizando eventos")
 	// Atualizando evento
 	@PutMapping("/{codigo}")
 	@Transactional
@@ -74,24 +90,29 @@ public class EventoController {
 			throws NotFoundException {
 		try {
 			EventoDto eventoDtoUpdate = eventoService.update(codigo, eventoDto);
+			logger.info("Evento com codigo: " + codigo + " atualizado");
 			return ResponseEntity.ok(eventoDtoUpdate);
 		} catch (Exception ex) {
 			ex.getMessage();
 			System.out.println("Evento não encontrado");
+			logger.info("Evento com codigo: " + codigo + " não encontrado");
 			return ResponseEntity.notFound().build();
 		}
 	}
 
+	@ApiOperation("Deletando eventos")
 	// Deletando evento
 	@DeleteMapping("/{codigo}")
 	@Transactional
 	public ResponseEntity<EventoDto> delete(@PathVariable Long codigo) {
 		try {
 			eventoService.delete(codigo);
+			logger.info("Evento com codigo: " + codigo + " deletado");
 			return ResponseEntity.ok().build();
 		} catch (NotFoundException ex) {
 			ex.printStackTrace();
 			System.out.println("Evento não encontrado");
+			logger.info("Evento com codigo: " + codigo + " não encontrado");
 			return ResponseEntity.notFound().build();
 		}
 	}
